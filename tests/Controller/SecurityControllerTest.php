204f9a1387637176739fd811447c42d5898b653c
geoffroy <geoffroy.dutot@jymeo.net>
2021-09-03 13:53:49 +0200

    Fonctionnal test on Homepage and Login page

---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,84 @@
+<?php
+
+
+namespace App\Tests\Controller;
+
+
+use App\Tests\Traits\LoginTest;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class SecurityControllerTest extends WebTestCase
+{
+    use LoginTest;
+
+    private KernelBrowser $client;
+
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
+
+    /**
+     * Test Login Page Form is http 200
+     */
+    public function testLoginPage()
+    {
+        $this->client->request('GET', '/login');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertSelectorTextContains('button', 'Se connecter');
+        $this->assertSelectorNotExists('.alert.alert-danger');
+    }
+
+    /**
+     * Test User already Auth try to access to Login Page, Redirect home
+     */
+    public function testLoginPageUserAuth()
+    {
+        $user = $this->getUser($this->client);
+        $this->login($this->client, $user);
+
+        $this->client->request('GET', '/login');
+
+        $this->assertResponseRedirects('/');
+        $this->client->followRedirect();
+        $this->assertSelectorTextContains('h1', 'Bienvenue sur Todo List, l\'application vous permettant de gérer l\'ensemble de vos tâches sans effort !');
+    }
+
+    /**
+     * Test Login Page Submit Bad data, Redirect to login and show error
+     */
+    public function testLoginBadCredentials()
+    {
+        $crawler = $this->client->request('GET', '/login');
+        $form = $crawler->selectButton('Se connecter')->form([
+            'username' => 'Username',
+            'password' => 'MyPassw0rd'
+        ]);
+        $this->client->submit($form);
+
+        $this->assertResponseRedirects('/login');
+        $this->client->followRedirect();
+        $this->assertSelectorExists('.alert.alert-danger');
+    }
+
+    /**
+     * Test Login Page Submit Valid data, Redirect to homepage
+     */
+    public function testSuccessLogin()
+    {
+        $crawler = $this->client->request('GET', '/login');
+        $form = $crawler->selectButton('Se connecter')->form([
+            'username' => 'user',
+            'password' => 'pass'
+        ]);
+        $this->client->submit($form);
+
+        $this->assertResponseRedirects('/');
+        $this->client->followRedirect();
+        $this->assertSelectorTextContains('h1', 'Bienvenue sur Todo List, l\'application vous permettant de gérer l\'ensemble de vos tâches sans effort !');
+    }
+}
+
---
+++ tests/Traits/LoginTest.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\Tests\Traits;
+
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+trait LoginTest {
+
+    /**
+     * Create user session
+     */
+    public function login(KernelBrowser $client, User $user) {
+        $session = $client->getContainer()->get('session');
+        $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+        $session->set('_security_main', serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $client->getCookieJar()->set($cookie);
+    }
+
+    /**
+     * Get a classic user
+     */
+    public function getUser(KernelBrowser $client) {
+        return $client->getContainer()->get('doctrine')->getRepository(User::class)->findOneBy(['roles' => ['']]);
+    }
+
+    /**
+     * Get an admin user
+     */
+    public function getAdminUser(KernelBrowser $client) {
+        return $client->getContainer()->get('doctrine')->getRepository(User::class)->findOneBy(['roles' => ['ROLE_ADMIN']]);
+    }
+}
